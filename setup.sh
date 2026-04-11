4e1be1abc0d38ed337b9a3cca252f034f9b462f8
Ahmed Chahi <ahmadachahi400@gmail.com>
2026-04-11 06:24:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ WORK_DIR="$TARGET_HOME/back"
 
 echo "[+] Starting setup for user: $TARGET_USER"
 
+echo "[+] Installing lsd"
+apt install -y lsd
+
+echo "[+] Installing cmatrix"
+
 # Create working directory
 echo "[+] Creating working directory at $WORK_DIR..."
 mkdir -p "$WORK_DIR"
@@ -34,16 +39,23 @@ usermod -s /bin/zsh $TARGET_USER
 # Move configuration files
 echo "[+] Moving configuration files..."
 apt install -y rsync
-mv -v /home/kali/back/setup/.zshrc /home/kali/
-mkdir /home/kali/.config 2>/dev/null
-rsync -a --delete /home/kali/back/setup/.config/ /home/kali/.config/
-mkdir /home/kali/.cache 2>/dev/null
-rsync  -a --delete  /home/kali/back/setup/.cache/ /home/kali/.cache/
-mkdir /home/kali/.mozilla 2>/dev/null
-mv -v "$WORK_DIR/setup/.mozilla/*" "$TARGET_HOME/.mozilla/"
-rm -rf /home/kali/.zsh_history
-mv -v /home/kali/back/setup/.zsh_history /home/kali/.zsh_history
-mv -v /home/kali/back/setup/fonts /home/kali/.local/share
+
+mv -v "$WORK_DIR/setup/.zshrc" "$TARGET_HOME/"
+
+mkdir -p "$TARGET_HOME/.config"
+rsync -a --delete "$WORK_DIR/setup/.config/" "$TARGET_HOME/.config/"
+
+mkdir -p "$TARGET_HOME/.cache"
+rsync -a --delete "$WORK_DIR/setup/.cache/" "$TARGET_HOME/.cache/"
+
+mkdir -p "$TARGET_HOME/.mozilla"
+cp -a "$WORK_DIR/setup/.mozilla/." "$TARGET_HOME/.mozilla/"
+
+rm -f "$TARGET_HOME/.zsh_history"
+mv -v "$WORK_DIR/setup/.zsh_history" "$TARGET_HOME/.zsh_history"
+
+mkdir -p "$TARGET_HOME/.local/share"
+cp -a "$WORK_DIR/setup/fonts" "$TARGET_HOME/.local/share/"
 
 
 
@@ -51,4 +63,12 @@ mv -v /home/kali/back/setup/fonts /home/kali/.local/share
 echo "[+] Fixing ownership..."
 chown -R $TARGET_USER:$TARGET_USER $TARGET_HOME
 
+echo "[+] Moving your wallpaper to downloads"
+cp -a "$WORK_DIR/setup/rack.jpg" "$TARGET_HOME/Downloads"
+
+echo "[+] Cleaning"
+rm -rf "$WORK_DIR"
+
 echo "[+] Setup completed successfully!"
+
+
